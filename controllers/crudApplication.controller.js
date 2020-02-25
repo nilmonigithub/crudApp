62ffc161d063cef348e1d709ac61baf4a5b9563d
npskp758 <pnilmoni.bluehorse@gmail.com>
2020-02-25 15:54:15 +0530

    complete other routes

--- controllers/crudApplication.controller.js
+++ controllers/crudApplication.controller.js
@@ -30,3 +30,87 @@ const student = {
         });
     });
 };
+
+// Retrieve all Students from the database.
+exports.findAll = (req, res) => {
+    const name = req.query.name;
+    var condition = name ? { name: { [Op.like]: `%${title}%` } } : null;
+  
+    Student.findAll({ where: condition })
+      .then(data => {
+        res.status(200).send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving Students."
+        });
+      });
+  };
+  
+  // Find a single Student with an id
+  exports.findOne = (req, res) => {
+    const id = req.params.id;
+  
+    Student.findByPk(id)
+      .then(data => {
+        res.status(200).send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving Student with id=" + id
+        });
+      });
+  };
+  
+  // Update a Student by the id in the request
+  exports.update = (req, res) => {
+    const id = req.params.id;
+  
+    Student.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Student was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update Student with id=${id}. Maybe Student was not found or req.body is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating Student with id=" + id
+        });
+      });
+  };
+  
+  // Delete a Student with the specified id in the request
+  exports.delete = (req, res) => {
+    const id = req.params.id;
+  
+    Student.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Student was deleted successfully!"
+          });
+        } else {
+          res.send({
+            message: `Cannot delete Student with id=${id}. Maybe Student was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete Student with id=" + id
+        });
+      });
+  };
+  
+  
--- routes/crudApplication.routes.js
+++ routes/crudApplication.routes.js
@@ -5,7 +5,18 @@ module.exports = app => {
     // Create a new Student
     router.post("/", student.create);
 
+    // Retrieve all Student
+    router.get("/", student.findAll);
+
+    // Retrieve a single Tutorial with id
+    router.get("/:id", student.findOne);
+
+    // Update a Student with id
+    router.put("/:id", student.update);
+
+    // Delete a Student with id
+    router.delete("/:id", student.delete);
   
-    app.use('/api/v1', router);
+    app.use('/api/v1/student', router);
 };
 
